d15e2d93a4c769e8e4d9f480221d840c399921fd
Chi Zhang <zhangch@jnist.cn>
2021-09-22 13:48:46 +0800

    :clown_face: sample

---
+++ sample/Maui.Toolkit.Sample/MauiProgram.cs
@@ -0,0 +1,35 @@
+﻿using Maui.Toolkit.WeChat;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace Maui.Toolkit.Sample
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+
+            builder
+                .UseMauiApp<App>()
+                .UseWeChat(new WeChatOption()
+                {
+                    AppId = "",
+                    AppSecret = ""
+                })
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+
+            builder.Services.AddSingleton<MainPage>();
+
+            return builder.Build();
+        }
+    }
+}
