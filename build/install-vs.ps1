c7a3d70efb1f1cef61e71ba8259472da632b9136
ZhangChi <zliming@outlook.com>
2021-09-24 18:25:29 +0800

    Update install-vs.ps1

--- build/install-vs.ps1
+++ build/install-vs.ps1
@@ -22,8 +22,8 @@ Invoke-WebRequest -UseBasicParsing `
     -OutFile "$env:TEMP\dd_vs_community.exe"
 
 Write-Host "Installing Visual Studio..."
-$exitCode = & "$env:TEMP\dd_vs_community.exe" --quiet --norestart --wait `
-  --add Microsoft.VisualStudio.Workload.NetCrossPlat;includeRecommended;includeOptional `
+$exitCode = & "$env:TEMP\dd_vs_community.exe" --quiet --norestart --wait --includeRecommended --includeOptional `
+  --add Microsoft.VisualStudio.Workload.NetCrossPlat `
   --add Microsoft.VisualStudio.Workload.NetCoreTools `
   --add Microsoft.VisualStudio.Workload.ManagedDesktop `
   --add Microsoft.VisualStudio.Workload.Universal `
