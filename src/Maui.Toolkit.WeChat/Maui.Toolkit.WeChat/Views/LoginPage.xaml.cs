425afcc4fe1a7d70ff6929e9ab8837fc4c63cb42
Chi Zhang <zhangch@jnist.cn>
2021-09-26 13:37:38 +0800

    :sparkles: login page

---
+++ src/Maui.Toolkit.WeChat/Maui.Toolkit.WeChat/Views/LoginPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Maui.Controls;
+
+
+namespace Maui.Toolkit.WeChat.Views;
+
+public partial class LoginPage : ContentPage
+{
+    public LoginPage()
+    {
+        InitializeComponent();
+    }
+}
