8b59cc56410141c53dfbf13d04632cde114bc0a7
Chi Zhang <zhangch@jnist.cn>
2021-09-28 23:02:04 +0800

    :white_check_mark: add unit test

--- src/Maui.Toolkit.WeChat/Maui.Toolkit.WeChat/WeChatOption.cs
+++ src/Maui.Toolkit.WeChat/Maui.Toolkit.WeChat/WeChatOption.cs
@@ -4,15 +4,37 @@ public class WeChatOption
 {
     public const string Name = "WeChat";
 
-    public string AppId { get; set; }
+    public string AppId { get; private set; }
 
-    public string AppSecret { get; set; }
+    public string AppSecret { get; private set; }
 
     public WeChatOption(
         string appId,
         string appSecret)
     {
-        AppId = appId;
-        AppSecret = appSecret;
+
+        AppId = CheckAppId(appId);
+        AppSecret = CheckAppSecret(appSecret);
+    }
+
+    private string CheckAppId(string appId)
+    {
+        if (string.IsNullOrWhiteSpace(appId))
+        {
+            throw new ArgumentNullException(nameof(appId));
+        }
+
+        return appId;
+    }
+
+
+    private string CheckAppSecret(string appSecret)
+    {
+        if (string.IsNullOrWhiteSpace(appSecret))
+        {
+            throw new ArgumentNullException(nameof(appSecret));
+        }
+
+        return appSecret;
     }
 }
