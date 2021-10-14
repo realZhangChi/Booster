56a9ad028779a0812a13c366e85dbab7aafae7a2
Chi Zhang <zhangch@jnist.cn>
2021-10-14 16:08:38 +0800

    :truck: remove test

--- test/Maui.Toolkit.WeChat/Maui.Toolkit.WeChat.Abstraction.UnitTest/Usings.cs
+++
@@ -1,7 +0,0 @@
-﻿global using Shouldly;
-global using System.Threading.Tasks;
-global using Xunit;
-global using Microsoft.Extensions.DependencyInjection;
-global using Maui.Toolkit.TestBase;
-global using Maui.Toolkit.WeChat.Services.Identity;
-global using Maui.Toolkit.WeChat.Models.Identity;
