bb6f5d6172f0a325ae8135a33f9bc2b3186794b7
Chi Zhang <zliming@outlook.com>
2021-10-18 15:29:02 +0800

    :heavy_minus_sign: remove source gen

---
+++ test/Maui.Toolkit.WeChat/Maui.Toolkit.WeChat.DeviceTest/Platforms/Android/TestInstrumentation.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Runtime;
+using Microsoft.Maui.TestUtils.DeviceTests.Runners.HeadlessRunner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maui.Toolkit.WeChat.DeviceTest
+{
+    [Instrumentation(Name = "com.companyname.maui.toolkit.wechat.devicetest.TestInstrumentation")]
+    public partial class TestInstrumentation : MauiTestInstrumentation
+    {
+        public TestInstrumentation(IntPtr handle, JniHandleOwnership ownership) : base(handle, ownership)
+        {
+        }
+    }
+}
--- test/Maui.Toolkit.WeChat/Maui.Toolkit.WeChat.DeviceTest/Test.cs
+++ test/Maui.Toolkit.WeChat/Maui.Toolkit.WeChat.DeviceTest/Test.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Maui;
 using Xunit;
+using FactAttribute = Microsoft.Maui.FactAttribute;
 
 namespace Maui.Toolkit.WeChat.DeviceTest
 {
@@ -12,7 +14,7 @@ namespace Maui.Toolkit.WeChat.DeviceTest
         [Fact]
         public void Should_Success()
         {
-            Assert.Equal(0, 0);
+            Assert.Equal(0, 1);
         }
     }
 }
