80d6b6b3f89722d94b3254b2b566be36b50b16dd
Chi Zhang <zliming@outlook.com>
2021-10-21 10:14:36 +0800

    :construction_worker: run device test

--- script/device-test.ps1
+++ script/device-test.ps1
@@ -1,6 +1,7 @@
 Get-ChildItem ./test/**/*-Signed.apk -recurse | ForEach-Object -Process{
 	if($_ -is [System.IO.FileInfo])
 	{
-		Write-Host($_.FullName);
+		$package = $_.Name -replace "-Signed.apk",""
+		xharness android test --output-directory=out --package-name=$package --app=$_ --instrumentation=$package.MauiTestInstrumentation --verbosity=Debug
 	}
 }
