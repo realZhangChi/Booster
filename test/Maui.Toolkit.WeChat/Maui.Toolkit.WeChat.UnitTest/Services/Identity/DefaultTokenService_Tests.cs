7903e70d7d27e110bb873288ea4e0045ec4e7f7e
Chi Zhang <zliming@outlook.com>
2021-10-22 17:18:23 +0800

    :art: remove whitespace line

--- test/Maui.Toolkit.WeChat/Maui.Toolkit.WeChat.UnitTest/Services/Identity/DefaultTokenService_Tests.cs
+++ test/Maui.Toolkit.WeChat/Maui.Toolkit.WeChat.UnitTest/Services/Identity/DefaultTokenService_Tests.cs
@@ -1,9 +1,6 @@
 ﻿using Maui.Toolkit.WeChat.Services.Http;
 using Shouldly;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
