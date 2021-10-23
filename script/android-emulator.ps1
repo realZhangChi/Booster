be5c1b29c190fe88c7a66504562a4ad60b5b0076
Chi Zhang <zliming@outlook.com>
2021-10-23 13:21:22 +0800

    :green_heart: fix script error

--- script/android-emulator.ps1
+++ script/android-emulator.ps1
@@ -1,15 +1,10 @@
-$avdManagerPath = (Join-Path $Env:ANDROID_SDK_ROOT, '/tools/bin')
-$emulatorPath = (Join-Path $Env:ANDROID_SDK_ROOT, '/emulator')
-Write-Host($avdManagerPath)
-Write-Host($emulatorPath)
-
-$avdManagerPath/avdmanager create avd -name android-31 --package "system-images;android-31;google_apis_playstore;x86_64"
+$Env:ANDROID_SDK_ROOT/tools/bin/avdmanager create avd -name android-31 --package "system-images;android-31;google_apis_playstore;x86_64"
 if (-Not $?) {
     Write-Host ("Avd creation failed.")
     exit $LASTEXITCODE
 }
 
-$emulatorPath/emulator -avd android-31 -no-window -gpu swiftshader_indirect -no-snapshot -noaudio -no-boot-anim
+$Env:ANDROID_SDK_ROOT/emulator/emulator -avd android-31 -no-window -gpu swiftshader_indirect -no-snapshot -noaudio -no-boot-anim
 if (-Not $?) {
     Write-Host ("Android emulator failed to start.")
     exit $LASTEXITCODE
