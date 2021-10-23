74a59d2ce8c693f5b9c038be2f47a5801ad10604
Chi Zhang <zliming@outlook.com>
2021-10-23 15:02:32 +0800

    :green_heart:

--- script/android-emulator.ps1
+++ script/android-emulator.ps1
@@ -3,15 +3,15 @@ $sdkRootPath = (Get-Item -Path $Env:ANDROID_SDK_ROOT -Verbose).FullName
 $toolsPath = (Join-Path $sdkRootPath "tools/bin")
 $emulatorPath = (Join-Path $sdkRootPath "emulator")
 
-Write-Host("toolsPath:" + $toolsPath)
-Write-Host("emulatorPath:" + $emulatorPath)
 Set-Location $sdkRootPath
 ls
 Set-Location $emulatorPath
+Write-Host("emulatorPath:" + $emulatorPath)
 ls
 Set-Location $toolsPath
+Write-Host("toolsPath:" + $toolsPath)
 ls
-avdmanager create avd -name android-31 --package "system-images;android-31;google_apis_playstore;x86_64"
+./avdmanager create avd -name android-31 --package "system-images;android-31;google_apis_playstore;x86_64"
 if (-Not $?) {
     Write-Host ("Avd creation failed.")
     exit $LASTEXITCODE
