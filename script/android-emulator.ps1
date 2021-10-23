c415f6c35530aa65edff6fdabfb253de84569919
Chi Zhang <zliming@outlook.com>
2021-10-23 16:10:29 +0800

    :green_heart:

--- script/android-emulator.ps1
+++ script/android-emulator.ps1
@@ -4,11 +4,13 @@ $toolsPath = (Join-Path $sdkRootPath "tools/bin")
 $emulatorPath = (Join-Path $sdkRootPath "emulator")
 
 Set-Location $toolsPath
-./sdkmanager "system-images;android-31;google_apis_playstore;x86_64" &
-yes |./sdkmanager --licenses &
-./avdmanager create avd --force -n android_31 -k "system-images;android-31;google_apis_playstore;x86_64"
+(./sdkmanager "system-images;android-31;google_apis_playstore;x86_64") -and
+(yes |./sdkmanager --licenses) -and
+(./avdmanager create avd --force -n android_31 -k "system-images;android-31;google_apis_playstore;x86_64")
 
 
 Set-Location $emulatorPath
 # ./emulator -avd android_31 -no-window -gpu swiftshader_indirect -no-snapshot -noaudio -no-boot-anim
 ls
+
+exit
