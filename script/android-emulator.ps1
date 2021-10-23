de18c1e5bfe6f28da0870f4f21788a919c6293b3
Chi Zhang <zliming@outlook.com>
2021-10-23 13:45:13 +0800

    path

--- script/android-emulator.ps1
+++ script/android-emulator.ps1
@@ -1,16 +1,17 @@
+Write-Host($Env:ANDROID_SDK_ROOT)
 $avdManagerPath = (Join-Path $Env:ANDROID_SDK_ROOT '/tools/bin').FullName
 $emulatorPath = (Join-Path $Env:ANDROID_SDK_ROOT '/emulator').FullName
 Write-Host($avdManagerPath)
 Write-Host($emulatorPath)
 
-Invoke-Expression -Command '$avdManagerPath/avdmanager create avd -name android-31 --package "system-images;android-31;google_apis_playstore;x86_64"'
-if (-Not $?) {
-    Write-Host ("Avd creation failed.")
-    exit $LASTEXITCODE
-}
-
-Invoke-Expression -Command '$emulatorPath/emulator -avd android-31 -no-window -gpu swiftshader_indirect -no-snapshot -noaudio -no-boot-anim'
-if (-Not $?) {
-    Write-Host ("Android emulator failed to start.")
-    exit $LASTEXITCODE
-}
+# Invoke-Expression -Command '$avdManagerPath/avdmanager create avd -name android-31 --package "system-images;android-31;google_apis_playstore;x86_64"'
+# if (-Not $?) {
+#     Write-Host ("Avd creation failed.")
+#     exit $LASTEXITCODE
+# }
+# 
+# Invoke-Expression -Command '$emulatorPath/emulator -avd android-31 -no-window -gpu swiftshader_indirect -no-snapshot -noaudio -no-boot-anim'
+# if (-Not $?) {
+#     Write-Host ("Android emulator failed to start.")
+#     exit $LASTEXITCODE
+# }
