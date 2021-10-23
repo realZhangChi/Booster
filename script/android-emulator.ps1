db35251b1f1278ba76e7e4dbe1f7f44230bda7a0
Chi Zhang <zliming@outlook.com>
2021-10-23 13:28:44 +0800

    :green_heart: Invoke-Expression

--- script/android-emulator.ps1
+++ script/android-emulator.ps1
@@ -1,10 +1,15 @@
-$Env:ANDROID_SDK_ROOT/tools/bin/avdmanager create avd -name android-31 --package "system-images;android-31;google_apis_playstore;x86_64"
+$avdManagerPath = (Join-Path $Env:ANDROID_SDK_ROOT, '/tools/bin').FullName
+$emulatorPath = (Join-Path $Env:ANDROID_SDK_ROOT, '/emulator').FullName
+Write-Host($avdManagerPath)
+Write-Host($emulatorPath)
+
+Invoke-Expression -Command '$avdManagerPath/avdmanager create avd -name android-31 --package "system-images;android-31;google_apis_playstore;x86_64"'
 if (-Not $?) {
     Write-Host ("Avd creation failed.")
     exit $LASTEXITCODE
 }
 
-$Env:ANDROID_SDK_ROOT/emulator/emulator -avd android-31 -no-window -gpu swiftshader_indirect -no-snapshot -noaudio -no-boot-anim
+Invoke-Expression -Command '$emulatorPath/emulator -avd android-31 -no-window -gpu swiftshader_indirect -no-snapshot -noaudio -no-boot-anim'
 if (-Not $?) {
     Write-Host ("Android emulator failed to start.")
     exit $LASTEXITCODE
