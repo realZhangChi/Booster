3aaea6f6e3c40f884af57be196069dbe5d70af9e
Chi Zhang <zliming@outlook.com>
2021-10-24 21:54:05 +0800

    :green_heart: use maui-check

--- script/android-emulator.ps1
+++ script/android-emulator.ps1
@@ -4,13 +4,13 @@ $toolsPath = (Join-Path $sdkRootPath "tools/bin")
 $emulatorPath = (Join-Path $sdkRootPath "emulator")
 
 Set-Location $toolsPath
-./avdmanager create avd --force -n android_31 -k "system-images;android-31;google_apis_playstore;x86_64" --device pixel
 ./avdmanager list avd
 ./avdmanager list device
 ./avdmanager list target
 
-# Set-Location $emulatorPath
-# # ./emulator -avd android_31 -no-window -gpu swiftshader_indirect -no-snapshot -noaudio -no-boot-anim
+Set-Location $emulatorPath
+./emulator -list-avds
+# ./emulator -avd android_31 -no-window -gpu swiftshader_indirect -no-snapshot -noaudio -no-boot-anim
 # ls
 
 exit
