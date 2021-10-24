4134b1e9f02be907a7e0178f1a77b1f1be6eee7e
Chi Zhang <zliming@outlook.com>
2021-10-24 22:00:18 +0800

    :green_heart: start emulator

--- script/android-emulator.ps1
+++ script/android-emulator.ps1
@@ -3,14 +3,8 @@ $sdkRootPath = (Get-Item -Path $Env:ANDROID_SDK_ROOT -Verbose).FullName
 $toolsPath = (Join-Path $sdkRootPath "tools/bin")
 $emulatorPath = (Join-Path $sdkRootPath "emulator")
 
-Set-Location $toolsPath
-./avdmanager list avd
-./avdmanager list device
-./avdmanager list target
-
 Set-Location $emulatorPath
-./emulator -list-avds
-# ./emulator -avd android_31 -no-window -gpu swiftshader_indirect -no-snapshot -noaudio -no-boot-anim
+./emulator -avd Android_Emulator_31 -no-window -gpu swiftshader_indirect -no-snapshot -noaudio -no-boot-anim
 # ls
 
-exit
+exit 0
