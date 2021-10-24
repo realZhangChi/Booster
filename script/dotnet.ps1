7e98ad5581da03a944f7fbc633f45b5a90f31810
Chi Zhang <zliming@outlook.com>
2021-10-24 22:22:32 +0800

    :green_heart: setup dotnet

--- script/dotnet.ps1
+++
@@ -1,4 +0,0 @@
-$ProgressPreference = 'SilentlyContinue'
-Invoke-WebRequest -Uri "https://dot.net/v1/dotnet-install.ps1" -OutFile dotnet-install.ps1
-& .\dotnet-install.ps1 -Version 6.0.100-rc.2.21505.57 -InstallDir "$env:ProgramFiles\dotnet\" -Verbose
-& dotnet --list-sdks
