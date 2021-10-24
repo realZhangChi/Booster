4756c3b239014dc6026a5bb8b1a87bbb76412d11
Chi Zhang <zliming@outlook.com>
2021-10-24 11:50:15 +0800

    :green_heart: list device

--- script/android-emulator.ps1
+++ script/android-emulator.ps1
@@ -4,11 +4,13 @@ $toolsPath = (Join-Path $sdkRootPath "tools/bin")
 $emulatorPath = (Join-Path $sdkRootPath "emulator")
 
 Set-Location $toolsPath
-./avdmanager create avd --force -n android_31 -k "system-images;android-31;google_apis_playstore;x86_64"
+./avdmanager create avd --verbose --force -n android_31 -k "system-images;android-31;google_apis_playstore;x86_64" --device "Pixel 5"
 ./avdmanager list avd
+./avdmanager list device
+./avdmanager list target
 
-Set-Location $emulatorPath
-# ./emulator -avd android_31 -no-window -gpu swiftshader_indirect -no-snapshot -noaudio -no-boot-anim
-ls
+# Set-Location $emulatorPath
+# # ./emulator -avd android_31 -no-window -gpu swiftshader_indirect -no-snapshot -noaudio -no-boot-anim
+# ls
 
 exit
