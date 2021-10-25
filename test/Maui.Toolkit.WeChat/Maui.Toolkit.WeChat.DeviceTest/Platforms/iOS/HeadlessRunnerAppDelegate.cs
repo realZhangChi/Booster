85d837da7b062c7261d7cfa03efab3046514b57a
Chi Zhang <zliming@outlook.com>
2021-10-25 13:31:49 +0800

    :construction_worker: create ios test runner

---
+++ test/Maui.Toolkit.WeChat/Maui.Toolkit.WeChat.DeviceTest/Platforms/iOS/HeadlessRunnerAppDelegate.cs
@@ -0,0 +1,12 @@
+﻿using Foundation;
+using Maui.Toolkit.DeviceTest.Runner;
+using Microsoft.Maui.Hosting;
+
+namespace Maui.Toolkit.WeChat.DeviceTest
+{
+    [Register("HeadlessRunnerAppDelegate")]
+    public class HeadlessRunnerAppDelegate : MauiTestApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
--- test/Maui.Toolkit.WeChat/Maui.Toolkit.WeChat.DeviceTest/Platforms/iOS/Program.cs
+++ test/Maui.Toolkit.WeChat/Maui.Toolkit.WeChat.DeviceTest/Platforms/iOS/Program.cs
@@ -9,7 +9,7 @@ namespace Maui.Toolkit.WeChat.DeviceTest
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
-            UIApplication.Main(args, null, typeof(AppDelegate));
+            UIApplication.Main(args, null, typeof(HeadlessRunnerAppDelegate));
         }
     }
 }
