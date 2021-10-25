a666c3b9b6bfc8f426a57e37d64d3a8fcc81172b
Chi Zhang <zliming@outlook.com>
2021-10-25 10:15:29 +0800

    :green_heart: android device test

--- script/android-emulator.ps1
+++ script/android-emulator.ps1
@@ -1,3 +1,4 @@
+$rootFolder = (Get-Item -Path "./" -Verbose).FullName
 $sdkRootPath = (Get-Item -Path $Env:ANDROID_SDK_ROOT -Verbose).FullName
 
 # $toolsPath = (Join-Path $sdkRootPath "tools/bin")
@@ -10,3 +11,16 @@ Set-Location $emulatorPath
 Set-Location $platformPath
 Write-Host ("adb devices:")
 adb devices
+
+Set-Location $rootFolder
+Get-ChildItem ./test/**/*-Signed.apk -recurse | ForEach-Object -Process{
+	if($_ -is [System.IO.FileInfo])
+	{
+		$package = $_.Name -replace "-Signed.apk",""
+		xharness android test --output-directory=out --package-name=$package --app=$_ --instrumentation=$package.TestInstrumentation --verbosity=Debug
+		if (-Not $?) {
+			Write-Host ("Test failed for the apk: " + $_.Name)
+			exit $LASTEXITCODE
+		}
+	}
+}
