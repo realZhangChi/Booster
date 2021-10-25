3a616729d12324341232d43fe76abef46943f3e0
Chi Zhang <zliming@outlook.com>
2021-10-25 12:09:05 +0800

    :green_heart: use android emulator runner

--- script/install-android-package.ps1
+++
@@ -1,5 +0,0 @@
-$sdkRootPath = (Get-Item -Path $Env:ANDROID_SDK_ROOT -Verbose).FullName
-$toolsPath = (Join-Path $sdkRootPath "tools/bin")
-
-Set-Location $toolsPath
-./avdmanager --install "system-images;android-31;google_apis_playstore;x86_64"
