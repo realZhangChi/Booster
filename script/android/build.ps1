ae2b6396d90efb49384e3ccd6e8ee23312983b60
Chi Zhang <zliming@outlook.com>
2021-10-26 21:40:33 +0800

    :green_heart: build android use debug

--- script/android/build.ps1
+++ script/android/build.ps1
@@ -3,7 +3,7 @@ $rootFolder = (Get-Item -Path "./" -Verbose).FullName
 Get-ChildItem ./test/**/*.DeviceTest.csproj -recurse | ForEach-Object -Process {
 
     Set-Location $_.Directory
-    dotnet build --configuration Release -f net6.0-android -p:EmbedAssembliesIntoApk=true
+    dotnet build --configuration Debug -f net6.0-android -p:EmbedAssembliesIntoApk=true
     if (-Not $?) {
         Write-Host ("Build failed for the app: " + $_.Name)
         exit $LASTEXITCODE
