43c49186778b55ebece3d92ab9aa5097ae16f5f7
Chi Zhang <zliming@outlook.com>
2021-10-30 21:28:05 +0800

    :building_construction: move bindings to src

--- script/nupkg/pack.ps1
+++ script/nupkg/pack.ps1
@@ -15,16 +15,3 @@ Get-ChildItem ./src/**/*.csproj -recurse | ForEach-Object -Process {
 }
 
 Set-Location $rootFolder
-Get-ChildItem ./binding/**/*.csproj -recurse | ForEach-Object -Process {
-    $projectFolder = $_.Directory
-
-    Set-Location $projectFolder
-    & dotnet pack -c Release
-    if (-Not $?) {
-        Write-Host ("Packaging failed for the project: " + $_.Name)
-        exit $LASTEXITCODE
-    }
-    Set-Location $rootFolder
-}
-
-Set-Location $rootFolder
