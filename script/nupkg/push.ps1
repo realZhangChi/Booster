a85e5544fc1a9e08969ee9414259bb6bc14f35ee
Chi Zhang <zliming@outlook.com>
2021-10-30 22:20:12 +0800

    :package: push

--- script/nupkg/push.ps1
+++ script/nupkg/push.ps1
@@ -1,11 +1,12 @@
 $apiKey = $args[0]
 
 $rootPath = (Get-Item -Path "./" -Verbose).FullName
+Write-Host ($rootPath)
 
-Get-ChildItem ./src/**/bin/Release/*.nupkg -recurse | ForEach-Object -Process {
-	Write-Host ($_)
+Get-ChildItem ./src/**/*.nupkg -recurse | ForEach-Object -Process {
 	if ($_ -is [System.IO.FileInfo]) {
 		$nupkgPath = $_.Directory
+		Write-Host ($nupkgPath)
 		Set-Location $nupkgPath
 		dotnet nuget push $_.Name -s https://api.nuget.org/v3/index.json --api-key "$apiKey"
 
