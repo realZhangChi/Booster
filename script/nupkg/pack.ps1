3de145615d8821b5a7197ad224ade24d50f384a8
Chi Zhang <zliming@outlook.com>
2021-10-30 09:48:00 +0800

    :green_heart: fix error

--- script/nupkg/pack.ps1
+++ script/nupkg/pack.ps1
@@ -6,7 +6,6 @@ Get-ChildItem ./src/**/*.csproj -recurse | ForEach-Object -Process {
     $projectFolder = $_.Directory
 
     Set-Location $projectFolder
-    Remove-Item -Recurse (Join-Path $projectFolder "bin/Release")
     & dotnet pack -c Release
     if (-Not $?) {
         Write-Host ("Packaging failed for the project: " + $_.Name)
