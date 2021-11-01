b7470a97440c5bbb68ae9ecd1476ebcd5b1a8160
Chi Zhang <zliming@outlook.com>
2021-11-01 21:47:38 +0800

    :sparkles: use source generator

--- sample/Booster.Sample/Platforms/Android/WeChatApi/WeChatEntryActivity.cs
+++
@@ -1,9 +0,0 @@
-﻿using Android.App;
-using Android.Content.PM;
-
-namespace Booster.Sample.Platforms.Android.WxApi;
-
-[Activity(Name = "Booster.Sample.WeChatApi.WeChatEntryActivity", Exported = true, TaskAffinity = "Booster.Sample", LaunchMode = LaunchMode.SingleTask)]
-public class WeChatEntryActivity : WeChat.Platforms.Android.WeChatApi.WeChatEntryActivity
-{
-}
