49410490e66a755306502dc96f593ff7b78534af
Chi Zhang <zliming@outlook.com>
2021-11-10 13:38:56 +0800

    :adhesive_bandage: source link

--- script/nupkg/push.ps1
+++ script/nupkg/push.ps1
@@ -6,7 +6,6 @@ Get-ChildItem ./src/**/*.nupkg -recurse | ForEach-Object -Process {
 	if ($_ -is [System.IO.FileInfo]) {
 		Set-Location $_.Directory
 		dotnet nuget push $_.Name -s https://api.nuget.org/v3/index.json --api-key "$apiKey"
-		dotnet nuget push $snupkg -s https://api.nuget.org/v3/index.json --api-key "$apiKey"
 
 		Set-Location $rootPath
 	}
